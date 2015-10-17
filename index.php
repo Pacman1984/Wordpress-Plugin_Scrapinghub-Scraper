a360caeeb232f45f0c7caa18685731a4b5c55f60
Sebastian <Sebastian@Sebastian-PC>
2015-10-17 04:18:24 +0200

    First Commit

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+/**
+ * Plugin Name: Scrapinghub Last Job Downloader
+ * Description: Downloads the last Job of Scrapionghub Spiders
+ * Version: 1.0
+ * Author: Sebastian Pachl
+ * License: ---
+ */
+if (! defined('ABSPATH')) exit;
+add_action( 'sp_wp_scrapinghub_xml_download', 'sp_scrapinghub_xml_download' );
+
+function sp_scrapinghub_xml_download() {
+    $apikey = 'YOUR-API-Key';
+    $project = 'YOUR-Project-ID';
+
+    $spiders = array(
+        'SpiderName_001',
+        'SpiderName_002',
+        'SpiderName_etc',
+    );
+    foreach ($spiders as $spider_name) {
+
+        $url = 'https://dash.scrapinghub.com/api/jobs/list.json?apikey=' .$apikey .'&project=' .$project .'&state=finished&spider=' .$spider_name; 
+        $jsondata = wp_remote_retrieve_body(wp_remote_get($url));
+        $json = json_decode($jsondata, true);
+        $lastid = $json['jobs'][0]['id'];
+
+        $request_url= "https://storage.scrapinghub.com/items/".$lastid ."?apikey=" .$apikey ."&format=xml";
+
+        $destination = plugin_dir_path( __FILE__ )."/uploads/" .$spider_name .".xml";
+        file_put_contents( $destination ,wp_remote_retrieve_body(wp_remote_get($request_url)));
+
+    }
+}
+?>
